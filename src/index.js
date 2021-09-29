6a941a18060d39ff068108649a1338282a15bab9
codebender <lionelmbankeu@gmail.com>
2021-09-30 00:09:57 +0200

    the playground is coming together

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-import canvas from  '@codebenderhq/canvas'
+import canvas from '@codebenderhq/canvas/'
+import MainApp from '@codebenderhq/canvas/src/components/app'
+import compone from '@codebenderhq/canvas/src/'
+import '@codebenderhq/canvas/src/style/index.css'
 
-canvas.app(() => (<div className="w-full h-screen flex justify-center">hello world</div>));
+
+
+
+console.log(canvas)
+canvas.app(() => (<MainApp><div name="one">hello world</div><div name="world">well what is the next issue</div> </MainApp>))
+
+// (() => {
+//     <App>
+//         <>hello world</>
+//     </App>
+// })
+// App(() => <>hello world</>)
+
+
+// app(() => (<Splash theme={"black"}></Splash>));
