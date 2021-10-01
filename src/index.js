1af8357c6479dada2fbbb1546f1b5966b713aebc
codebender <lionelmbankeu@gmail.com>
2021-10-01 13:07:14 +0200

    created a chappies playground example

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
-import {Sauveur} from '@codebenderhq/canvas/'
-// import MainApp from '@codebenderhq/canvas/src/theme/sauveur/components/app' 
-import '@codebenderhq/canvas/src/theme/sauveur/style/index.css'
+import { Sauveur, Chappies } from "@codebenderhq/canvas/";
+// import MainApp from '@codebenderhq/canvas/src/theme/sauveur/components/app'
+import "@codebenderhq/canvas/src/theme/sauveur/style/index.css";
 
-
-
-
-
-console.log(Sauveur)
-Sauveur.app(() => (<Sauveur.MainApp><div name="one">hello world</div><div name="world">well what is the next issue</div> </Sauveur.MainApp>))
+Sauveur.app(() => (
+  <Chappies.Layout>
+    <Chappies.LayoutCol>
+        <div className="text-8xl p-8 text-center font-chappies-bold ">
+            <span className="text-blue-500">C</span>
+            <span className="text-red-500">h</span>
+            <span className="text-yellow-300">a</span>
+            <span className="text-blue-500">p</span>
+            <span className="text-green-500">p</span> 
+            <span className="text-red-500">i</span>
+            <span className="text-yellow-300">e</span>
+            <span className="text-green-500">s</span>
+        </div>
+      <Chappies.Search></Chappies.Search>
+ 
+    </Chappies.LayoutCol>
+    <div className="w-full h-full absolute flex items-end justify-center z-0 object-bottom p-2 font-chappies-regular select-none">
+        A design by <a href="https://twitter.com/DonovanGoliath" className="mx-1">@DonovanGoliath</a>
+    </div>
+ 
+  </Chappies.Layout>
+));
 
 // (() => {
 //     <App>
@@ -15,6 +31,5 @@ Sauveur.app(() => (<Sauveur.MainApp><div name="one">hello world</div><div name="
 //     </App>
 // })
 // App(() => <>hello world</>)
-
 
 // app(() => (<Splash theme={"black"}></Splash>));
